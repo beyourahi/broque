196c9751d4800393978705b8a67cdc3ba0ef0be8
beyourahi <beyourahi@gmail.com>
2024-11-13 10:08:40 +0600

    Add new user to permitted users and update package dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ const Home = async () => {
     const permitted_users = [
         "beyourahi@gmail.com",
         "rahikhan360@gmail.com",
+        "preetyfarihaafreen@gmail.com"
     ];
 
     if (!isAuthenticated || !permitted_users.includes(user?.email!)) redirect("/login");
